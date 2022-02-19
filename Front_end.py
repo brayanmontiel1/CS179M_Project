4ae741d9ec856ae3f6da65d37b8c3cff9cae4d64
brayanmontiel1 <67293845+brayanmontiel1@users.noreply.github.com>
2022-02-18 19:19:50 -0800

    pushing working window switches, login, job selection screen, and manifest upload

--- Front_end.py
+++ Front_end.py
@@ -4,23 +4,23 @@ from turtle import heading
 import PySimpleGUI as sg
 import json
 import datetime
+import os
 import string
 
+from numpy import empty
 
+#THIS IS THE FRONT END CODE FOR CS179 PROJECT IN ARTIFICAL INTELLIGENCE
+#WINTER 2022 
 
 sg.theme('DefaultNoMoreNagging') 
 #font 
 global heading_font, body_font
 #selectedJob : None = 0, Load/unload = 1, Balancing = 2 
 global selectedJob
-#global variables cor user, login time, and full name
-global currUser, loginTime, fullName
+#global variables incudes current user username, full name-
+#login time and manifest name
+global currUser, loginTime, fullName , manifest
 
-#for themes
-#https://www.reddit.com/r/Python/comments/dxule8/color_themes_pysimplegui_adds_significantly_more/
-
-#importing users - users.json contains user info and login credentials
-# Opening JSON file
 heading_font = ("Arial, 24")
 body_font = ("Arial, 14")
 users_file = open('SAIL_Project/CS179M_Project/users.json')
@@ -52,9 +52,9 @@ def getTimeandDate():   ##working
     #print(date_time)
     return date_time
 
-#---------------LOGIN PAGE LAYOUT METHOD------------------------------
-def login(users_array): #working --- needs touch up
-    #currentUser : user that is logged in 
+#---------------INITIAL LOGIN USER METHOD------------------------------------
+def login(): 
+    #currUser : user that is logged in 
     #loginTime : date and time of login in military format
     #center items using columns : [sg.Column([ ], justification='center')]
     #adjust filename if needed for your pc -- Remember to change at production time
@@ -66,35 +66,30 @@ def login(users_array): #working --- needs touch up
                 [sg.Column([[sg.Input(justification='center', key='-usrnm-')]], justification='center')], 
                 [sg.Column([[sg.Button('Login'), sg.Button('Exit')]], justification='center')],
             ]
+    return sg.Window("SAIL ENTERPRISE - LOGIN", layout, size=(1000, 700), resizable=True, grab_anywhere=True, margins=(0, 0), finalize=True)
 
-    window = sg.Window("SAIL ENTERPRISE - LOGIN", layout, size=(1000, 700), resizable=True, grab_anywhere=True, margins=(0, 0))
-
-    while True:
-        event,values = window.read()
-        if event == "Exit" or event == sg.WIN_CLOSED:
-            print('Exit message -- window closed')
-            break ##change this back to menu page or login page depending if theres existing user
-        else:
-            if event == "Login":
-                if values['-usrnm-'] in users_array: ##---sucessful login
-                    currUser = values['-usrnm-']
-                    fullName = getFullName(currUser)
-                    loginTime = getTimeandDate()
-                    print(fullName)
-                    print(loginTime)
-                    sg.popup("Welcome " , fullName) 
-                    window.close()  ##close current window and start selectJob process
-                    selectJob(currUser, fullName, loginTime)
-                elif values['-usrnm-'] not in users_array: ##---invalid login
-                    sg.popup("Invalid login. Try again")
-            #break
-    #selectJob(currUser, fullName, loginTime) -- have to figure out how to launch after login
-    window.close()
-
-#---------------JOB SELECTION LAYOUT METHOD------------------------------
-def selectJob(currUser, fullName, loginTime): ##semi working --- need to format
+#---------------SIGN OFF->ON NEW USER METHOD------------------------------------
+def loginNew(): 
+    #currUser : user that is logged in 
+    #loginTime : date and time of login in military format
+    #center items using columns : [sg.Column([ ], justification='center')]
+    #adjust filename if needed for your pc -- Remember to change at production time
     my_img = sg.Image(filename='SAIL_Project/CS179M_Project/img/SaIL.png', key='-sail_logo-')
+    
     layout =[
+                [sg.Column([[sg.Text('Current User: ' + fullName ,font=body_font)]], justification='left')],  
+                [sg.Column([[sg.Text('Login Time: ' + str(loginTime), font=body_font)]], justification='left')],  
+                [sg.Column([[my_img]], justification='center')],
+                [sg.Column([[sg.Text('Enter username: ', font=body_font)]], justification='center')],  
+                [sg.Column([[sg.Input(justification='center', key='-usrnm-')]], justification='center')], 
+                [sg.Column([[sg.Button('Login'), sg.Button('Go Back')]], justification='center')],
+            ]
+    return sg.Window("SAIL ENTERPRISE - LOGIN", layout, size=(1000, 700), resizable=True, grab_anywhere=True, margins=(0, 0), finalize=True)
+
+#---------------JOB SELECTION METHOD------------------------------------
+def selectJob(): 
+    my_img = sg.Image(filename='SAIL_Project/CS179M_Project/img/SaIL.png', key='-sail_logo-')
+    layout1 =[
                 [sg.Column([[sg.Text('Current User: ' + fullName ,font=body_font)]], justification='left')],  
                 [sg.Column([[sg.Text('Login Time: ' + str(loginTime), font=body_font)]], justification='left')],  
                 [sg.Column([[my_img]], justification='center')],
@@ -103,89 +98,91 @@ def selectJob(currUser, fullName, loginTime): ##semi working --- need to format
                 [sg.Column([[sg.Button('Start New Balancing Job')]], justification='center')],
                 [sg.Column([[sg.Button('Login Another User')]], justification='center')],
             ]
+    return sg.Window("SAIL ENTERPRISE - Select Job", layout1, size=(1000, 700), resizable=True, grab_anywhere=True, margins=(0, 0), finalize=True)
 
-    window = sg.Window("SAIL ENTERPRISE - Select Job", layout, size=(1000, 700), resizable=True, grab_anywhere=True, margins=(0, 0))
-    trigger1 = 0
-    while True:
-        event = window.read()
-        #selectedJob : Load/unload = 1, Balancing = 2
-        # login just goes back to login screen 
-        #  ^will use to know what job is selected at upload manifest layout
-        if event == 'Start New Load/Unload':
-            selectedJob = 1
-            trigger1 = 1
-            print('Start new load/unload -- forward to upload manifest/n')
-            break
-        elif event == 'Start New Balancing Job':
-            selectedJob = 2
-            trigger1 = 1
-            print('Start new balancing job -- forward to upload manifest/n')
-            break
-        elif event == 'Login Another User': 
-            print('forward to login screen')
-            trigger1 = 2
-            login(auth_users)  ##once user clicks cant go back unless login
-        elif event == sg.WIN_CLOSED:
-            print('Exit message -- window closed')
-            break #exits below
-    if trigger1 == 1:
-        window.close()
-        uploadManifest()
-    elif trigger1 == 2:
-        window.close()
-        login(auth_users)
-    elif trigger1 == 0:
-        window.close()
-
-    
-    
-    
-
-#---------------JOB SELECTION LAYOUT METHOD------------------------------
-def uploadManifest(): ##semi working --- need to format
+#---------------UPLOAD MANIFEST METHOD------------------------------------
+def uploadManifest(): 
     layout =[
                 [sg.Column([[sg.Text('Current User: ' + fullName ,font=body_font)]], justification='left')],  
                 [sg.Column([[sg.Text('Login Time: ' + str(loginTime), font=body_font)]], justification='left')],  
                 [sg.Column([[sg.Text('\n\n Upload Manifest: ', font=heading_font)]], justification='center')],   
                 [sg.Column([[sg.Text("Choose a file: "), sg.Input(), sg.FileBrowse(key="-manifest-")]], justification='center')],  
-                [sg.Column([[sg.Button('Submit')]], justification='center')],
-                [sg.Column([[sg.Button('Cancel')]], justification='center')],
+                [sg.Column([[sg.Button('Submit Manifest')]], justification='center')],
+                [sg.Column([[sg.Button('Cancel Upload')]], justification='center')],
             ]
+    return sg.Window("SAIL ENTERPRISE - Select Job", layout, size=(1000, 700), resizable=True, grab_anywhere=True, margins=(0, 0), finalize=True)
 
-    window = sg.Window("SAIL ENTERPRISE - Select Job", layout, size=(1000, 700), resizable=True, grab_anywhere=True, margins=(0, 0))
 
-    while True:
-        event = window.read()
-        #selectedJob : Load/unload = 1, Balancing = 2, Login = 3 
-        #  ^will use to know what job is selected at upload manifest layout
-        if event == sg.WIN_CLOSED:
-            break #exits below
-        else:
-            if event == "Submit":
-                if selectedJob == 1:
-                    sg.popup("Start balance loading job") #Placeholder - forward to load/unload layout
-                elif selectedJob == 2: 
-                    sg.popup("Start transfer job") #Placeholder - forward to balancing layout 
-                break
-            elif event == "Cancel":
-                selectedJob = 0 #restore job selection
-                window.close()  ##close current window and go back to selectJob layout
-                selectJob(currUser, fullName, loginTime)
+window1, selectJobWindow, uploadWindow = login(), None, None    # start off with login window open (window 1)
+
+while True:             # Event Loop
+    window, event, values = sg.read_all_windows()
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        window.close()
+        if window == selectJobWindow:       # if closing selectJobWindow, mark as closed
+            selectJobWindow = None
             break
-    window.close()
+        elif window == uploadWindow:       
+            uploadWindow = None
+            break
+        elif window == window1:     # if closing win 1, exit program
+            break
 
-#infinte running window -- have to CTRL+C then ENTER to exit program in terminal
-""" while True:
-    getTimeandDate()
-    login(auth_users)
-    #event, values = window.read()
-    #print(event, values)
-    #if event == None or event == 'Exit':     # If user closed window with X or if user clicked "Exit" button then exit
-    #    break
-    #window.close()   """
+    ##LOGIN process for initial and new user     
+    if event == "Login":
+        if values['-usrnm-'] in auth_users: ##---sucessful login
+            currUser = values['-usrnm-']
+            fullName = getFullName(currUser)
+            loginTime = getTimeandDate()
+            print("Full Name: ", fullName)
+            print("Username: ", currUser)
+            print("Login Time: ",loginTime)
+            print('Login Successful - Foward to JOB SELECTION')
+            sg.popup("Welcome!" , fullName) 
+            window1.Hide()
+            selectJobWindow = selectJob()       #REDIRECT to job selection window
+        elif values['-usrnm-'] not in auth_users: ##---invalid login
+            sg.popup("Invalid Login. Try again")  
+    ##goes back if dont want to sign in new user
+    elif event == 'Go Back':
+        print('Login Canceled - Return to JOB SELECTION')
+        window1.Hide()
+        selectJobWindow.UnHide() ##show job selection window for current user again
+    
+    ##JOB SELECTION process
+    elif event == 'Start New Load/Unload':
+        selectedJob = 1
+        selectJobWindow.Hide() #closes job selection window
+        print('LOAD/UNLOAD -- Forward to UPLOAD MANIFEST/n')
+        selectJobWindow.Hide() 
+        uploadWindow = uploadManifest()
+    elif event == 'Start New Balancing Job':
+        selectedJob = 2
+        print('BALANCING -- Forward to UPLOAD MANIFEST/n')
+        selectJobWindow.Hide()
+        uploadWindow = uploadManifest()
+    elif event == 'Login Another User':     ##REDIRECT to main login
+        print('Forward to LOGIN screen')
+        selectJobWindow.Hide() 
+        window1 = loginNew()   #load login window
 
-#REGULAR MAIN()
-if __name__ == "__main__":
-    login(auth_users)
+    ##UPLOAD manifest process
+    elif event == "Submit Manifest":
+        manifest = values['-manifest-'] #save manifest name
+        if(manifest == ''):
+            print("INVALID MANIFEST - Pop message to try again.")
+            sg.popup("Empty selection, try again.") #Error message if empty
+        else:
+            manifest = os.path.basename(manifest)
+            print('SELECTED MANIFEST : ', manifest)
+            if selectedJob == 1:
+                sg.popup("Starting new Loading Job") #Placeholder - forward to load/unload layout
+            elif selectedJob == 2: 
+                sg.popup("Start new Balancing Job") #Placeholder - forward to balancing layout 
+    elif event == "Cancel Upload":
+        print("CANCLED Manifest upload - RETURN to Job Selection")
+        selectedJob = 0 #restore job selection
+        uploadWindow.Hide()
+        selectJobWindow.UnHide()
 
-
+window.close()
